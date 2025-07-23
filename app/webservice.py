78c61d7d0e4f475ed0a5f3422383724d250aa771
Nabeegh-Ahmed <nabeegh08@gmail.com>
2025-07-24 02:06:28 +0500

    duration

--- app/webservice.py
+++ app/webservice.py
@@ -135,8 +135,8 @@ class SourceUriBody(BaseModel):
 
 @app.post("/asr-source-uri", tags=["Endpoints"])
 async def asr_source_uri(body: SourceUriBody):
-    audio_file = requests.get(body.source_uri)
-    audio_file = io.BytesIO(audio_file.content)
+    audio_file_raw = requests.get(body.source_uri)
+    audio_file = io.BytesIO(audio_file_raw.content)
     result = asr_model.transcribe(
         load_audio(audio_file, True),
         "transcribe",
@@ -147,6 +147,8 @@ async def asr_source_uri(body: SourceUriBody):
         None,
         "json",
     )
+    result["duration"] = 0
+
     return StreamingResponse(
         result,
         media_type="application/json",
